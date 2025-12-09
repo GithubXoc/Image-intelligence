2414c834e9ec55efb15d780f532e04690d88baba
Batsukh Khosbayar <bhosoo0523@gmail.com>
2025-12-10 00:37:12 +0900

    initialize s3 file upload

---
+++ server/services/storage.py
@@ -0,0 +1,33 @@
+from fastapi import UploadFile
+from dotenv import load_dotenv
+
+import boto3
+import os
+
+load_dotenv()
+
+BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_REGION = os.getenv("AWS_REGION", "ap-northeast-1")
+
+s3_client = boto3.client("s3", region_name=AWS_REGION, 
+                         aws_access_key_id=AWS_ACCESS_KEY_ID,
+                         aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
+def upload_file_to_s3(file_location: str, file_name: str, file: UploadFile, is_analyzed: bool):
+    try:
+        extra_args = {'ContentType': file.content_type}
+        
+        is_analyzed_str = 'true' if is_analyzed else 'false'
+        extra_args['Tagging'] = f'analyze={is_analyzed_str}'
+        
+        s3_client.upload_fileobj(
+            Fileobj=open(file_location, "rb"),
+            Bucket=BUCKET_NAME,
+            Key=file_name,
+            ExtraArgs=extra_args
+        )
+    except Exception as e:
+        print(f"Error uploading file to S3: {e}")
+        raise e
